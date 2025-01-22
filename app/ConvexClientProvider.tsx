a9f4e3d855bc97abde133fb7944a98708d9dd595
KARTIKEY-KATYAL <107783109+KARTIKEY-KATYAL@users.noreply.github.com>
2025-01-22 14:20:16 +0530

    ok

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -2,6 +2,13 @@
 import { ReactNode } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 
+// Ensure NEXT_PUBLIC_CONVEX_URL is available
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  console.error(
+    "NEXT_PUBLIC_CONVEX_URL is not defined in the environment variables."
+  );
+}
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,14 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <Head>
-        <Script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-          strategy="lazyOnload"
-          crossOrigin="anonymous"
-        />
-      </Head>
+       
       <body className={inter.className}>
         <ConvexClientProvider>
          {children}
